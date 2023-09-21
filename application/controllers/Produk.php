d3eaedfca8f10f5570a75d2c4549e7285225c7b8
celticc <andrewanto27@gmail.com>
2023-09-21 07:34:15 +0700

    21-09

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -3,14 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Produk extends CI_Controller {
 
+    // mengakses produk_model
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Produk_Model');
+        
+    }
+    
+    // menampilkan data di tabel produk
     public function index()
     {
         $data = array(
+            'produk' =>$this->Produk_Model->Beranda(),
             'isi' => 'pelanggan/produk'
         );
         $this->load->view('pelanggan/layout/wrapper', $data, FALSE);
-        
-        
     }
 
 }
--- application/views/pelanggan/produk.php
+++ application/views/pelanggan/produk.php
@@ -1,8 +1,13 @@
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> 
+<title>ASSA GALERI</title>
+<div class="row">
+  <?php foreach ($produk as $prd): ?>
+    <div class="card mr-3 mb-3" style="width: 18rem;">
+    <img class="card-img-top" style="display: block; margin-left: auto; margin-right: auto; margin-top:15px; width:250px; height:250px;" src="<?php echo base_url().'uploads/'.$prd->foto ?>" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $prd->nama ?></h5>
+      <span>Rp. <?php echo $prd->harga ?></span><br>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div> 
+  <?php endforeach; ?>
+</div>
