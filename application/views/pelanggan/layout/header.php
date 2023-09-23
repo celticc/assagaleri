809206a0027d8e75f974b56606e23773df5aceda
celticc <andrewanto27@gmail.com>
2023-09-23 09:27:02 +0700

    23-09

--- application/views/pelanggan/layout/header.php
+++ application/views/pelanggan/layout/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-
+    <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzDHPihbUCvrHPS-HeWgxhYcVXK4jV9MiZ1Q&usqp=CAU" type="image/x-icon">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- application/views/pelanggan/produk.php
+++ application/views/pelanggan/produk.php
@@ -1,4 +1,5 @@
 <title>ASSA GALERI</title>
+
 <div class="row">
   <?php foreach ($produk as $prd): ?>
     <div class="card mr-3 mb-3" style="width: 18rem;">
@@ -6,8 +7,38 @@
     <div class="card-body">
       <h5 class="card-title"><?php echo $prd->nama ?></h5>
       <span>Rp. <?php echo $prd->harga ?></span><br>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div style="text-align: center; margin-top:5px;">
+      <a href="#" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Beli"  ><i class="fas fa-fw fa-cart-arrow-down"></i></a>
+      <a href="#" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Deskripsi"  ><i class="fas fa-fw fa-eye" data-toggle="modal" data-target="#produk_<?php echo $prd->id ?>"></i></a>
+      <a href="#" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Tambahkan ke keranjang"  ><i class="fas fa-fw fa-cart-plus"></i></a>    
+      </div>   
     </div>
   </div> 
   <?php endforeach; ?>
+</div>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
